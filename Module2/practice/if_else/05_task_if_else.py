f4bb7288c224b926d21bd86be185053e21e73ff8
Elena Bochkova <planeado@gmail.com>
2022-02-26 16:29:49 +0300

    Update 05_task_if_else.py

--- Module2/practice/if_else/05_task_if_else.py
+++ Module2/practice/if_else/05_task_if_else.py
@@ -4,3 +4,14 @@
 # Формат выходных данных: требуется вывести название времени года
 
 # TODO: your code here
+
+month = int(input("Введите номер месяца: "))
+
+if month > 2 and month < 6:
+    print("Spring")
+elif month > 5 and month < 9:
+    print("Summer")
+elif month > 8 and month < 12:
+    print("Autumn")
+else:
+    print("Winter")
