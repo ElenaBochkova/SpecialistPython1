2c06e3c669a695f9475715d4a2394482ddfcaa0b
Elena Bochkova <planeado@gmail.com>
2022-03-07 14:55:19 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -4,4 +4,18 @@
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
-# TODO: your code here
+index = 1
+count = 0
+
+while index != len(text):
+    index = text.find(" ")
+    if index == -1:
+        index = len(text)
+        word = text
+    else:
+        word = text[:index]
+    if len(word) > 5:
+        count += 1
+    text = text[index+1:]
+
+print(count)
