538e06daf98583288aa2a3bd95ebee08eb08abfe
Elena Bochkova <planeado@gmail.com>
2022-03-07 16:31:54 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,13 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+my_list = []
+
+i = first_number + 1
+
+while i < second_number:
+    if i % 3 == 0:
+        my_list.append(i)
+    i += 1
+
+print(my_list)
