1cdecaa44d831f8628ff509080b28a93f40a454c
Elena Bochkova <planeado@gmail.com>
2022-03-12 00:43:38 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -3,4 +3,14 @@
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
+name_length = 0
+the_longest = ""
+
+for el in names:
+    if len(el) > name_length:
+        the_longest = el
+        name_length = len(el)
+
+print(the_longest)
+
 # TODO: your code here
