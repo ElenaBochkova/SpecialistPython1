0e500b03ba673c0176461e2363b9d22fc67e0ffc
Elena Bochkova <planeado@gmail.com>
2022-03-12 00:40:32 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -4,6 +4,14 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+names_in_line = ""
+
+for el in names:
+    if names_in_line != "":
+        names_in_line += ", "
+    names_in_line += el
+
+print(names_in_line)
 
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
