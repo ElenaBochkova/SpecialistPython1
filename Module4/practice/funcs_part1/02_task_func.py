1e4ba1aab56e20064f6658337ddf947b81448226
Elena Bochkova <planeado@gmail.com>
2022-03-12 14:53:09 +0300

    Update 02_task_func.py

--- Module4/practice/funcs_part1/02_task_func.py
+++ Module4/practice/funcs_part1/02_task_func.py
@@ -3,8 +3,23 @@
 # Пример палиндрома: 34543
 # * попробуйте решить данную задачу, не преобразуя число к строке
 
+def my_len(number):
+    for i in range(1, number):
+        if number // 10 ** i == 0:
+            return i
+    return 0
+
+
 def palindrome(number):
-    pass
+    while my_len(number) > 0:
+        first = number // 10 ** (my_len(number) - 1)
+        second = number % 10
+        if first != second:
+            return False
+        number = number - first * (10 ** (my_len(number) - 1))
+        number = number // 10
+    return True
+
 
 
 # Тестируем функцию
