7a6dffa8ba42280841f8872305678cb398e0538c
Elena Bochkova <planeado@gmail.com>
2022-03-19 11:19:28 +0300

    Update 04_task_repeat.py

--- Module5/practice/repeat/04_task_repeat.py
+++ Module5/practice/repeat/04_task_repeat.py
@@ -6,5 +6,9 @@
 # веб-странице и вывода списка остальных страниц.
 
 def pagination(num_items, items_on_page):
-    pass
+    if (num_items % items_on_page) == 0:
+        return int(num_items/items_on_page)
+    else:
+        return int(num_items/items_on_page) + 1
 
+print(pagination(49, 10))
