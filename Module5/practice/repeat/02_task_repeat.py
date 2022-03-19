8d582670dcce15a3f8bd3490cb420eb4e586651e
Elena Bochkova <planeado@gmail.com>
2022-03-19 11:06:25 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -3,5 +3,21 @@
 # палиндром - число одинаково читающееся слева направо, так и справа налево.
 #  Пример палиндрома: 12321
 
+def my_len(number):
+    for i in range(number):
+        if number // 10 ** i == 0:
+            return i
+    return 0
+
+
 def palindrome(number):
-    pass
+    while my_len(number) > 0:
+        first = number // 10 ** (my_len(number) - 1)
+        second = number % 10
+        if first != second:
+            return False
+        number = number - first * (10 ** (my_len(number) - 1))
+        number = number // 10
+    return True
+
+print(palindrome(12321))
