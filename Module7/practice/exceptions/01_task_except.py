c002817d80a36622352e140597cf0287104a1c52
Elena Bochkova <planeado@gmail.com>
2022-03-19 16:47:45 +0300

    Update 01_task_except.py

--- Module7/practice/exceptions/01_task_except.py
+++ Module7/practice/exceptions/01_task_except.py
@@ -4,3 +4,24 @@
 # На вход программе подается строка формата nxm (x - латинская буква икс).
 # Пример входных данных: 12x6
 # Если данные вводятся в неверном формате, сообщить об этом и запросить ввод заново.
+
+def validate(a, b):
+    try:
+        a = int(a)
+        b = int(b)
+        return a, b
+    except ValueError:
+        print("Data cannot be transformed to int")
+
+
+while True:
+    line = input("Data: ")
+    try:
+        a, b = line.split("x")
+        a, b = validate(a, b)
+        print("Квадратов:", a // b)
+        break
+    except ValueError:
+        print("Data are incorrect")
+    except TypeError:
+        print("Data are incorrect")
