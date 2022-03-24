00762dce738cd869f5b7b0a61d9a50c543a7c200
Elena Bochkova <planeado@gmail.com>
2022-03-24 23:16:46 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,25 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+while True:
+    line = input("Введите пять целых чисел: ")
+    line_list = line.split(" ")
+    number_list = []
+    try:
+        for number in line_list:
+            number_list.append(int(number))
+        if len(number_list) > 5:
+            raise ValueError
+        break
+    except ValueError:
+        print("Данные некорректные. Попробуйте ввести их снова.")
+
+min_positive = number_list[0]
+for number in number_list:
+    if 0 <= number < min_positive:
+        min_positive = number
+if min_positive >= 0:
+    print("Минимальное положительное число из введенных", min_positive)
+else:
+    print("Вы не ввели положительных чисел")
