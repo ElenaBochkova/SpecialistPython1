9f785aff39dc93034558c7e2bf467b6b3608afa3
Elena Bochkova <planeado@gmail.com>
2022-03-24 23:24:19 +0300

    Update 02_hw_file.py

--- Module6/home_work/02_hw_file.py
+++ Module6/home_work/02_hw_file.py
@@ -1,5 +1,13 @@
 # Дан файл data/info.txt, в каждой строке которого содержится строка или целое число
 # Найдите сумму всех чисел, пропуская все строки содержащие не числовые значения
 
+sum = 0
 with open("data/info.txt", "r") as f:
-    pass
+    for line in f:
+        try:
+            number = int(line)
+        except ValueError:
+            number = 0
+        sum += number
+
+print(sum)
