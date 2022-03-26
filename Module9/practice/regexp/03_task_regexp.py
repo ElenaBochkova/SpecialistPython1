4219b19c9d0ee4b39cda4d8442992f8c2543a108
Elena Bochkova <planeado@gmail.com>
2022-03-26 16:42:21 +0300

    Update 03_task_regexp.py

--- Module9/practice/regexp/03_task_regexp.py
+++ Module9/practice/regexp/03_task_regexp.py
@@ -5,3 +5,18 @@
 # example.com — Нет
 # https://кремль.рф — Да
 
+import re
+
+text_list = ["http://re.re",
+        "https://re/re",
+        "https://кремль.рф",
+        "example.com"]
+
+pattern1 = r"http://\w+[.]\w+"
+pattern2 = r"https://\w+[.]\w+"
+
+for text in text_list:
+    if re.fullmatch(pattern1, text) or re.fullmatch(pattern2, text):
+        print("Yes")
+    else:
+        print("No")
